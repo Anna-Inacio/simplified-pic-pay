cf8c854cb1cc87db1ca28c4b62d5f8940def9b08
Anna Inacio <anna.inacio@a.fecaf.com.br>
2025-08-19 23:33:54 -0300

    First commit

---
+++ simplified-pic-pay/src/main/java/com/simplified_pic_pay/SimplifiedPicPayApplication.java
@@ -0,0 +1,13 @@
+package com.simplified_pic_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPicPayApplication.class, args);
+	}
+
+}
---
+++ simplified-pic-pay/src/test/java/com/simplified_pic_pay/SimplifiedPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplified_pic_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
