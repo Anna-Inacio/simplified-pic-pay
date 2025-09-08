89f15b6eec3fa4257f74c5b3dd3238f03dd907bb
Anna Inacio <anna.inacio@a.fecaf.com.br>
2025-09-07 23:35:04 -0300

    Add swagger configs

--- simplified-pic-pay/src/main/java/com/simplified_pic_pay/controller/TransactionController.java
+++ simplified-pic-pay/src/main/java/com/simplified_pic_pay/controller/TransactionController.java
@@ -3,6 +3,9 @@ package com.simplified_pic_pay.controller;
 import com.simplified_pic_pay.domain.transaction.Transaction;
 import com.simplified_pic_pay.dtos.TransactionDTO;
 import com.simplified_pic_pay.service.transaction.TransactionService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,13 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created transaction"),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content()),
+            @ApiResponse(responseCode = "400", description = "Insufficient Balance", content = @Content()),
+            @ApiResponse(responseCode = "400", description = "Merchants cannot send money", content = @Content()),
+            @ApiResponse(responseCode = "403", description = "Transaction unauthorized", content = @Content()),
+    })
     @PostMapping
     public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) {
         var newTransaction = transactionService.createTransaction(transaction);
--- simplified-pic-pay/src/main/java/com/simplified_pic_pay/controller/UserController.java
+++ simplified-pic-pay/src/main/java/com/simplified_pic_pay/controller/UserController.java
@@ -4,6 +4,8 @@ import com.simplified_pic_pay.domain.user.User;
 import com.simplified_pic_pay.dtos.UserDTO;
 import com.simplified_pic_pay.repository.UserRepository;
 import com.simplified_pic_pay.service.user.UserService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +25,9 @@ public class UserController {
     @Autowired
     private UserRepository repository;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created user")
+    })
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
         var newUser = userService.createUser(user);
