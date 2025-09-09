ab75300fef840f968b3f29c7e6c8a63f08933992
Anna Inacio <anna.inacio@a.fecaf.com.br>
2025-09-09 13:08:20 -0300

    Adjust swagger

--- simplified-pic-pay/src/main/java/com/simplified_pic_pay/dtos/TransactionDTO.java
+++ simplified-pic-pay/src/main/java/com/simplified_pic_pay/dtos/TransactionDTO.java
@@ -1,8 +1,14 @@
 package com.simplified_pic_pay.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.math.BigDecimal;
 
-public record TransactionDTO (BigDecimal amount,
-                              Long senderId,
-                              Long receiverId) {
+public record TransactionDTO(
+        @Schema(example = "100.00")
+        BigDecimal amount,
+        @Schema(example = "1")
+        Long senderId,
+        @Schema(example = "2")
+        Long receiverId) {
 }
--- simplified-pic-pay/src/main/java/com/simplified_pic_pay/dtos/UserDTO.java
+++ simplified-pic-pay/src/main/java/com/simplified_pic_pay/dtos/UserDTO.java
@@ -1,14 +1,23 @@
 package com.simplified_pic_pay.dtos;
 
 import com.simplified_pic_pay.domain.user.UserType;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.math.BigDecimal;
 
-public record UserDTO (String firstName,
-                       String lastName,
-                       String document,
-                       String email,
-                       String password,
-                       BigDecimal balance,
-                       UserType userType) {
+public record UserDTO(
+        @Schema(example = "Anna")
+        String firstName,
+        @Schema(example = "Banana")
+        String lastName,
+        @Schema(example = "12345678900")
+        String document,
+        @Schema(example = "anna.banana@gmail.com")
+        String email,
+        @Schema(example = "P@ssw0rd!")
+        String password,
+        @Schema(example = "1000.00")
+        BigDecimal balance,
+        @Schema(example = "COMMON")
+        UserType userType) {
 }
